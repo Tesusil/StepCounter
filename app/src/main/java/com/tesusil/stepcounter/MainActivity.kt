7e7faa3a7c658d20123851b5b3be1c9feff4ec55
Tesusil <kamil_fracala@wp.pl>
2021-04-19 17:47:35 +0200

    Created a layout of the main activity. Enabled data binding.

--- app/src/main/java/com/tesusil/stepcounter/MainActivity.kt
+++ app/src/main/java/com/tesusil/stepcounter/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.tesusil.stepcounter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.tesusil.stepcounter.databinding.ActivityMainBinding
+import com.tesusil.stepcounter.view.MainBindingModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.model = MainBindingModel(2500,10000)
+        binding.mainProgressBar.progress = 25
     }
 }
---
+++ app/src/main/java/com/tesusil/stepcounter/view/MainBindingModel.kt
@@ -0,0 +1,6 @@
+package com.tesusil.stepcounter.view
+
+data class MainBindingModel(
+    val steps: Int = 0,
+    val maxSteps: Int = 10000
+)
